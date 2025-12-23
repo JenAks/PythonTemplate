b64f8ffee1f07aa04486168809d30187cd7a3626
Jenny Aksenova <jenny.asealsecurity.io@Jennys-MacBook-Pro.local>
2025-12-23 14:59:19 +0200

    merge python template to github

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jenny.asealsecurity.io/SealTests/PythonTemplate/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
